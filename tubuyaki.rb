7783a0ac5d39303d54ce74a73688f1dea7467008
Takayuki Kamiyama <karuma.reason@gmail.com>
2017-02-18 16:26:28 +0900

    speed performance up.

--- tubuyaki.rb
+++ tubuyaki.rb
@@ -6,14 +6,14 @@ module Tubuyaki
 
 def version
 require 'tubuyaki/version'
-print "Tubuyaki_Version : "
+print 'Tubuyaki_Version : '
 puts Tubuyaki::VERSION
 end
 
 def start
-puts "取扱説明書".center(60,'-')
-puts ""
-puts "
+puts '取扱説明書'.center(60,'-')
+puts ''
+puts '
 
 >tubuyaki
 　∟tubuyakiを起動します
@@ -26,9 +26,9 @@ puts "
 
 tubuyakiはMITライセンスです
 
-"
-puts""
-puts "取扱説明書".center(60,'-')
+'
+puts''
+puts '取扱説明書'.center(60,'-')
 end
 
 def soft
@@ -46,15 +46,15 @@ title 'boyaki is shout!'
 }
     
 TkLabel.new(nil,
-:text => "ひとりごと...顔だけキン肉マン...かおだけきん...",
-:fg => "blue").pack
+:text => 'ひとりごと...顔だけキン肉マン...かおだけきん...',
+:fg => 'blue').pack
 #家族、曰く私が眠くなるという独り言らしい
 
 entry = TkEntry.new.pack('side'=>'left')
 
 c = TkButton.new{
-  image TkPhotoImage.new("file" => "tweet_icon.gif")
-  pack('fill'=>'x',"padx"=>2.5,"pady"=>2.5)
+  image TkPhotoImage.new('file' => 'tweet_icon.gif')
+  pack('fill'=>'x','padx'=>2.5,'pady'=>2.5)
 }
 
 c.command proc {
@@ -64,57 +64,57 @@ c.command proc {
  case saikoro
 
  when 4 then
- File.open("backup.txt", "a:sjis") do |f|
+ File.open('backup.txt', 'a:sjis') do |f|
  TkLabel.new(window1,
-        :text => f.puts("#{entry.value}"),
-        :text => ("#{entry.value}"),
-        :fg => "black",
-        :bg => "yellow").pack('fill'=>'x',"padx" => 2.5,"pady" => 2.5)
+        :text => f.puts('#{entry.value}'),
+        :text => ('#{entry.value}'),
+        :fg => 'black',
+        :bg => 'yellow').pack('fill'=>'x','padx' => 2.5,'pady' => 2.5)
     end
     
  when 3 then   
- File.open("backup.txt", "a:sjis") do |f|
+ File.open('backup.txt', 'a:sjis') do |f|
  TkLabel.new(window1,
-        :text => f.puts("#{entry.value}"),
-        :text => ("#{entry.value}"),
-        :fg => "black",
-        :bg => "white").pack('fill'=>'x',"padx" => 2.5,"pady" => 2.5)
+        :text => f.puts('#{entry.value}'),
+        :text => ('#{entry.value}'),
+        :fg => 'black',
+        :bg => 'white').pack('fill'=>'x','padx' => 2.5,'pady' => 2.5)
     end
     
  when 2 then
- File.open("backup.txt", "a:sjis") do |f|
+ File.open('backup.txt', 'a:sjis') do |f|
  TkLabel.new(window1,
-        :text => f.puts("#{entry.value}"),
-        :text => ("#{entry.value}"),
-        :fg => "white",
-        :bg => "black").pack('fill'=>'x',"padx" => 2.5,"pady" => 2.5)
+        :text => f.puts('#{entry.value}'),
+        :text => ('#{entry.value}'),
+        :fg => 'white',
+        :bg => 'black').pack('fill'=>'x','padx' => 2.5,'pady' => 2.5)
 end	
 	
  when 1 then
- File.open("backup.txt", "a:sjis") do |f|
+ File.open('backup.txt', 'a:sjis') do |f|
  TkLabel.new(window1,
-        :text => f.puts("#{entry.value}"),
-        :text => ("#{entry.value}"),
-        :fg => "black",
-        :bg => "green").pack('fill'=>'x',"padx" => 2.5,"pady" => 2.5)
+        :text => f.puts('#{entry.value}'),
+        :text => ('#{entry.value}'),
+        :fg => 'black',
+        :bg => 'green').pack('fill'=>'x','padx' => 2.5,'pady' => 2.5)
 end	
 	
 when 0 then
-File.open("backup.txt", "a:sjis") do |f|
+File.open('backup.txt', 'a:sjis') do |f|
  TkLabel.new(window1,
-        :text => f.puts("#{entry.value}"),
-        :text => ("#{entry.value}"),
-        :fg => "white",
-        :bg => "red").pack('fill'=>'x',"padx" => 2.5,"pady" => 2.5)
+        :text => f.puts('#{entry.value}'),
+        :text => ('#{entry.value}'),
+        :fg => 'white',
+        :bg => 'red').pack('fill'=>'x','padx' => 2.5,'pady' => 2.5)
     end
     
 else
-   File.open("backup.txt", "a:sjis") do |f|
+   File.open('backup.txt', 'a:sjis') do |f|
    TkLabel.new(window1,
-        :text => f.puts("#{entry.value}"),
-        :text => ("#{entry.value}"),
-        :fg => "white",
-        :bg => "blue").pack('fill'=>'x',"padx" => 2.5,"pady" => 2.5)
+        :text => f.puts('#{entry.value}'),
+        :text => ('#{entry.value}'),
+        :fg => 'white',
+        :bg => 'blue').pack('fill'=>'x','padx' => 2.5,'pady' => 2.5)
     end
     
 end
@@ -146,18 +146,18 @@ when one.match(he)
 Tubuyaki.start
 else
 puts ''
-puts "Please choose an option."
-puts "Dear my wife."
+puts 'Please choose an option.'
+puts 'Dear my wife.'
 puts ''
 end
 
-# require "bundler/setup"
+# require 'bundler/setup'
 # You can add fixtures and/or initialization code here to make experimenting
 # with your gem easier. You can also use a different console, if you like.
 
 # (If you use this, don't forget to add pry to your Gemfile!)
-# require "pry"
+# require 'pry'
 # Pry.start
 
-# require "irb"
+# require 'irb'
 # IRB.start
